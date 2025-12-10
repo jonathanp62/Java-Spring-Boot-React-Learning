b567dbca8e403fa23d0689cbed185d20d2458115
Jonathan Parker <jonathanp62@gmail.com>
2025-12-10 06:26:12 -0500

    Initial 0.1.0 commit

---
+++ src/main/java/net/jmp/spring/boot/react/learning/bankkycform/FormController.java
@@ -0,0 +1,73 @@
+package net.jmp.spring.boot.react.learning.bankkycform;
+
+/*
+ * (#)FormController.java   0.1.0   12/10/2025
+ *
+ * @author    Jonathan Parker
+ * @version   0.1.0
+ * @since     0.1.0
+ *
+ * MIT License
+ *
+ * Copyright (c) 2025 Jonathan M. Parker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import static net.jmp.util.logging.LoggerUtils.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+/// The person controller.
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/react/learning/api/bank-kyc-form")
+public class FormController {
+    /// The logger.
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /// The default constructor.
+    public FormController() {
+        super();
+    }
+
+    /// The OK method.
+    ///
+    /// @return org.springframework.http.ResponseEntity<java.lang.String>
+    @GetMapping("/ok")
+    public ResponseEntity<String> ok() {
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(entry());
+        }
+
+        final ResponseEntity<String> result = new ResponseEntity<>("OK", HttpStatus.OK);
+
+        if (this.logger.isTraceEnabled()) {
+            this.logger.trace(exitWith(result));
+        }
+
+        return result;
+    }
+}
