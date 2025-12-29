8f6806e992bc11a92daaa4c2ba94b41b207ef773
Jonathan Parker <jonathanp62@gmail.com>
2025-12-29 14:25:05 -0500

    Added more scripts for the order endpoints

---
+++ bin/get-order.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env zsh
+
+# (#)get-order.sh 0.1.0   12/29/2025
+#
+# @author   Jonathan Parker
+# @version  0.1.0
+# @since    0.1.0
+#
+# MIT License
+#
+# Copyright (c) 2025 Jonathan M. Parker
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Usage:
+#       get-sales-tax-name.sh <order-id>
+
+if [ "$#" -eq 0 ] || [ "$#" -gt 1 ]
+then
+        echo "Usage: $0 <order-id>"
+        exit 1
+fi
+
+SITE="http://localhost:8080"
+
+curl -i -X GET ${SITE}/react/learning/api/e-commerce/order/${1} \
+  -H "Accept: application/json" \
