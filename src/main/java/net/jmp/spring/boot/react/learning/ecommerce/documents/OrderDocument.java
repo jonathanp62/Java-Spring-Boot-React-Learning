c9dd8a5f2edc178f1822b2bde3e4cfe53389af82
Jonathan Parker <jonathanp62@gmail.com>
2026-03-07 16:04:40 -0500

    ECO-090 Updated some commentary

--- src/main/java/net/jmp/spring/boot/react/learning/ecommerce/documents/OrderDocument.java
+++ src/main/java/net/jmp/spring/boot/react/learning/ecommerce/documents/OrderDocument.java
@@ -10,7 +10,7 @@ package net.jmp.spring.boot.react.learning.ecommerce.documents;
  *
  * MIT License
  *
- * Copyright (c) 2025 Jonathan M. Parker
+ * Copyright (c) 2025, 2026 Jonathan M. Parker
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
