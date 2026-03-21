dda75e5203c7ee7dd31df124f8cfecdef22c335e
Jonathan Parker <jonathanp62@gmail.com>
2026-03-21 08:01:51 -0400

    ECO-102 Created the SolrConfigurer class which returns an initialized Solr client

---
+++ src/main/java/net/jmp/spring/boot/react/learning/configuration/SolrConfigurer.java
@@ -0,0 +1,54 @@
+package net.jmp.spring.boot.react.learning.configuration;
+
+/*
+ * (#)SolrConfigurer.java   0.4.0   03/21/2026
+ *
+ * @author    Jonathan Parker
+ * @version   0.4.0
+ * @since     0.4.0
+ *
+ * MIT License
+ *
+ * Copyright (c) 2026 Jonathan M. Parker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import org.apache.solr.client.solrj.impl.HttpJdkSolrClient;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/// The solr configurer
+@Configuration
+public class SolrConfigurer {
+    /// The Solr URL
+    @Value("${solr.url:http://localhost:8983/solr}")
+    private String solrUrl;
+
+    /// The Solr client
+    ///
+    /// @return org.apache.solr.client.solrj.impl.HttpJdkSolrClient
+    @Bean
+    public HttpJdkSolrClient solrClient() {
+        return new HttpJdkSolrClient.Builder(this.solrUrl).build();
+    }
+}
